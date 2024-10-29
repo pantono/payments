b158f4f6323722859dfbcfa9e619406fbdebe202
Chris Burton <chris.burton@revolutionbarsgroup.com>
2024-10-29 22:37:36 +0000

    Initial Commit

---
+++ migrations/20241018194109_payments.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Payments extends AbstractMigration
+{
+    public function change(): void
+    {
+        
+    }
+}
